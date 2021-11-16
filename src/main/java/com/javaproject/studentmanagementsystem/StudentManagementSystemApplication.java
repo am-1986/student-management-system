c39dd3c904fb3a266e5ab590c5b7582c97bf6922
Ashish <ashish_mohanty10@yahoo.com>
2021-11-16 13:13:03 +0530

    added list student feature

--- src/main/java/com/javaproject/studentmanagementsystem/StudentManagementSystemApplication.java
+++ src/main/java/com/javaproject/studentmanagementsystem/StudentManagementSystemApplication.java
@@ -1,13 +1,37 @@
 package com.javaproject.studentmanagementsystem;
 
+import com.javaproject.studentmanagementsystem.entity.Student;
+import com.javaproject.studentmanagementsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentManagementSystemApplication {
+public class StudentManagementSystemApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentManagementSystemApplication.class, args);
     }
 
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentManagementSystemApplication(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Student student1 = new Student("Priyanshi", "Pattanaik", "priyanshi@gmail.com");
+        studentRepository.save(student1);
+
+        Student student2 = new Student("Anushka", "Sharma", "anushka@gmail.com");
+        studentRepository.save(student2);
+
+        Student student3 = new Student("Tony", "Stark", "tonystark@gmail.com");
+        studentRepository.save(student3);
+    }
 }
